eb7cb5137f4eab129befc33044e3b2a67d2f8231
suhangdev <17816876697@163.com>
2020-03-29 23:35:05 +0800

    commit

--- blog/components/creative/creative.tsx
+++ blog/components/creative/creative.tsx
@@ -2,10 +2,26 @@ import React from 'react'
 import Card from '../card/card'
 import './creative.less'
 
+import { EnvironmentOutlined } from '@ant-design/icons'
+
 const Creative: React.FunctionComponent = () => {
+    const handleFollowClick = () => {
+        window.open('https://github.com/suhangdev')
+    }
     return (
         <Card>
-            Creative
+            <div className="creative">
+                <img className="creative-avatar" src="//suhangdev.su.bcebos.com/avatar.png"/>
+                <div className="creative-name">suhangdev</div>
+                <div className="creative-profession">Web Developer</div>
+                <div className="creative-location">
+                    <EnvironmentOutlined className="creative-location-icon" />
+                    <span>中国 浙江 杭州</span>
+                </div>
+                <div className="creative-follow" onClick={handleFollowClick}>
+                    <span>Follow</span>
+                </div>
+            </div>
         </Card>
     )
 }
--- node/config/config.default.ts
+++ node/config/config.default.ts
@@ -1,25 +1,43 @@
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
 
 export default (appInfo: EggAppInfo) => {
-  const config = {} as PowerPartial<EggAppConfig>;
+    const config = {} as PowerPartial<EggAppConfig>;
 
-  // override config from framework / plugin
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1580633168284_1419';
+    // override config from framework / plugin
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1580633168284_1419';
 
-  // add your egg config in here
-  config.middleware = [];
+    // add your egg config in here
+    config.middleware = [];
 
-  // add your special config in here
-  const bizConfig = {
-    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
-  };
+    // add your special config in here
+    const bizConfig = {
+        sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+    };
 
-  config.mysql = require('/var/www/suhang/mysqlconfig.json')
+    // config.mysql = require('/var/www/suhang/mysqlconfig.json')
 
-  // the return config will combines to EggAppConfig
-  return {
-    ...config,
-    ...bizConfig,
-  };
+    config.mysql = {
+        client: {
+            host: '106.13.201.201',
+            port: '3306',
+            user: 'root',
+            password: 'suhangdev',
+            database: 'blog',
+        },
+    }
+    config.security = {
+        csrf: { enable: false },
+        domainWhiteList: ['*']
+    };
+    config.cors = {
+        origin: 'http://localhost:3000',
+        credentials: true,   // 开启认证
+        allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS'
+    };
+    // the return config will combines to EggAppConfig
+    return {
+        ...config,
+        ...bizConfig,
+    };
 };
