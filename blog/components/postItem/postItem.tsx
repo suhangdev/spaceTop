f9a7e98aec3e02b92a0b1d09266e61b6a17b0e2e
suhangdev <17816876697@163.com>
2020-05-03 22:30:05 +0800

    date

--- blog/components/postItem/postItem.tsx
+++ blog/components/postItem/postItem.tsx
@@ -7,11 +7,19 @@ import { ListItem } from '../../type/types'
 
 const PostItem: React.FunctionComponent<{data: ListItem}> = ({data}) => {
     console.log(data)
+    const { ctime } = data
+    const date: Date = new Date(ctime)
+    console.log(date.getFullYear())
     return(
         <Link href="/post/[id]" as={`post/${data.id}`}>
             <a>
                 <Card hover={true}>
                     <h2>{data.title}</h2>
+                    <div className="postItem-info">
+                        <div>
+                            <span>{`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`}</span>
+                        </div>
+                    </div>
                 </Card>
             </a>
         </Link>
