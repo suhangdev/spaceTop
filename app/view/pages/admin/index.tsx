578eea29fd66312167485613b514bb8f2271f796
suhang <17816876697@163.com>
2020-09-24 10:21:10 +0800

    feat: add

---
+++ app/view/pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.less';
+
+const Admin: React.FC<{}> = () => {
+  return (
+    <div>
+      <h1 className={styles.title}>Page Admin</h1>
+    </div>
+  );
+};
+
+export default Admin;
