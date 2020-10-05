1bc3c05be471c1e4b5864bc099b3fda3aeb415da
suhangdev <17816876697@163.com>
2020-10-06 01:21:34 +0800

    feat: init

---
+++ egg/config/config.default.ts
@@ -0,0 +1,28 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1601917004846_9140';
+
+  // add your egg config in here
+  config.middleware = [];
+
+  config.cors = {
+    origin: 'http://localhost:8000',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  }
+
+  // add your special config in here
+  const bizConfig = {
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig,
+  };
+};
---
+++ umi/src/models/posts.ts
@@ -0,0 +1,13 @@
+const postsModel = {
+    namespace: 'posts',
+    state: {
+        postName: '',
+    },
+    reducers: {
+        save(state: any, action: any) {
+            return 'asd'
+        },
+    },
+}
+
+export default postsModel
